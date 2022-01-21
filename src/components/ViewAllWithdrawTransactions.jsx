70e3f66d8e944839893725d6bc855c07bed9ffec
stitchineer <stitchineer@yahoo.com>
2022-01-21 11:57:41 -0500

    something wrong with API request

---
+++ src/components/ViewAllWithdrawTransactions.jsx
@@ -0,0 +1,61 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export default function ViewAllWithdrawTransactions() {
+    //instead of showing description, can show green or red according to whether it is deposit or withdraw
+    //list by most recent date
+
+    //i would really like to set this up as a modularization component
+    //pass in any transactions JSON as props and format it as table
+    const transactionsAPI = 'http://localhost:8081/transactions';
+    const [transactions, setTransactions] = useState([]);
+  //  const [accountID, setTransactions] = useState([]);
+
+    useEffect(() => {
+        axios.get(transactionsAPI+'/withdraw', {
+            accountID: 1
+        })
+            .then((response) => {
+                console.log(response.data);
+                setTransactions(response.data);
+            })
+    }, []);
+
+    return (
+        <>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Transaction #</th>
+                        <th>User</th>
+                        <th>Account #</th>
+                        <th>Amount</th>
+                        <th>Date</th>
+                        <th>Type</th>
+                        <th>Note</th>
+                    </tr>
+                </thead>
+                {transactions.map(d => {
+                    return (
+                        <tr>
+                            <td>{d.transactionID}</td>
+                            <td>{d.userID}</td>
+                            <td>{d.accountID}</td>
+                            <td>${d.amount}</td>
+                            <td>{d.transactionDate}</td>
+                            <td>{d.transactionType}</td>
+                            <td>{d.transactionNote}</td>
+                        </tr>
+                    )
+                })}
+            </table>
+        </>
+    )
+
+
+
+
+
+
+
+}
