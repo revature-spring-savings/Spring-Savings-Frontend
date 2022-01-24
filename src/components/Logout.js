08a35921e25ec6632eb26d6947ad3827ac14fec6
HannahHowell <hrhowell59@hotmail.com>
2022-01-24 14:37:45 -0600

    added video home and logout pages

---
+++ src/components/Logout.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Navbar from '../../../Navbar';
+import { Button } from '../../../buttons/Button';
+import './Logout.css';
+import '../../../../App.css';
+import LogoutVideo from '../../../videos/LogoutVideo';
+function Logout() {
+    // sessionStorage.setItem('name', newName);
+    // sessionStorage.setItem('username', uname);
+    sessionStorage.removeItem('name');
+    sessionStorage.removeItem('username');
+    return (
+       <>
+       <Navbar/>
+
+       <LogoutVideo/>
+       <Button/>
+       </>
+    )
+}
+
+export default Logout
