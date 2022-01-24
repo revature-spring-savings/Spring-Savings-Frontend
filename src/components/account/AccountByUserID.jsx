3e65f7bf076e43af0bd070ba0088241821e9ac1c
Colleen Malone <stitchineer@yahoo.com>
2022-01-24 17:32:55 -0500

    Update AccountByUserID.jsx

--- src/components/account/AccountByUserID.jsx
+++ src/components/account/AccountByUserID.jsx
@@ -1,12 +1,9 @@
 import axios from 'axios';
-<<<<<<< HEAD:src/components/AccountByUserIDApi.jsx
+
 import React, {useState, useEffect} from 'react';
 
 
 export const AccountByUserIDConst =()=>{
-=======
-import React, { useState, useEffect } from 'react';
->>>>>>> origin/team-1:src/components/account/AccountByUserID.jsx
 
 export const AccountByUserIDConst = () => {
     const [account, setAccount] = useState([]);
@@ -23,7 +20,6 @@ export const AccountByUserIDConst = () => {
     <>  
         {account.map(({accountID, accountType, accountBalance}, index) =>{
             return (
-<<<<<<< HEAD:src/components/AccountByUserIDApi.jsx
                 <div key={index} class="acctCard">
                     <div class="acctCardHeader">
                         <h3>Account: {accountID}</h3>
@@ -36,13 +32,7 @@ export const AccountByUserIDConst = () => {
                         {/* <button class="viewMore" id={accountID}>View More</button> */}
                         <p>View Recent Transactions</p>
                     </div>
-                
-=======
-                <div key={index}>
-                    <h3>Account: {accountID}</h3>
-                    <p>Type: {accountType}</p>
-                    <p>Balance: {accountBalance}</p>
->>>>>>> origin/team-1:src/components/account/AccountByUserID.jsx
+
                 </div>
             )
         })}
