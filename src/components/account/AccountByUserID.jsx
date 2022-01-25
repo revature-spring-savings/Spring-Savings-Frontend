0757fb8a1070cbecb6b9b5fa9e0e5a32f27095ae
stitchineer <stitchineer@yahoo.com>
2022-01-25 15:24:58 -0500

    hide show

--- src/components/account/AccountByUserID.jsx
+++ src/components/account/AccountByUserID.jsx
@@ -1,49 +1,55 @@
 import axios from 'axios';
 
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import CloseButton from '../dashboard/CloseButton';
 
 export const AccountByUserID = () => {
     const [account, setAccount] = useState([]);
+    const [showMoreInfo, setShowMoreInfo] = useState(0);
 
-    useEffect(()=>{
+  //  const handleShowMoreInfo = () => setShowMoreInfo(!showMoreInfo);
+
+    useEffect(() => {
         var userID = 2;
-        axios.get(`http://localhost:8081/accounts/${userID}/all-accounts`).then(res =>{
+        axios.get(`http://localhost:8081/accounts/${userID}/all-accounts`).then(res => {
             console.log(res);
             setAccount(res.data);
-        });   
-    },[]);
+        });
+    }, []);
 
-    function closerLook(accountID){
-        console.log(accountID+" was clicked");
-        document.getElementById(accountID).innerHTML = accountID;
-        <CloseButton accountID={this.accountID} />
+    const accountMap = account.map(({ accountID, accountType, accountBalance }, index) => {
 
-    }
+        return (
+            <div key={index} className="acctCard">
+                <div className="acctCardHeader">
+                    <h3>Account: {accountID}</h3>
+                </div>
 
-    return (
-    <>  
-        {account.map(({accountID, accountType, accountBalance}, index) =>{
-            return (
-                <div key={index} class="acctCard">
-                    <div class="acctCardHeader">
-                        <h3>Account: {accountID}</h3>
-                    </div>
+                <p>Type: {accountType}</p>
+                <p>Balance: ${accountBalance}</p>
 
-                    <p>Type: {accountType}</p>
-                    <p>Balance: ${accountBalance}</p>
+                <div className="acctCardFooter" id={accountID} onClick={(e) => setShowMoreInfo(e.target.accountID)}>
+                     {/* <button class="viewMore" id={accountID} onClick={setShowMoreInfo(accountID)}>View More</button> */}
+                  {/*  onClick => add className to hide and unhide
+                    */}
+                    <p>View More Details</p>
+                    {(showMoreInfo == accountID) ? <></> :
 
-                    <div className="acctCardFooter" onClick={function(e){closerLook(accountID)}} >
-                        {/* <button class="viewMore" id={accountID}>View More</button> */}
-                        <p>View Recent Transactions</p>
-                    </div>
-                    <div className="moreDetails" id={accountID}></div>
-                    {/* <div className="moreDetails" id={accountID}><CloseButton accountID={accountID} /></div> */}
+                        <p>accountID is {accountID}</p>
+                    }
 
                 </div>
-            )
-        })}
-    </>
+                {/* <div className="moreDetails" id={accountID}></div> */}
+                {/* <div className="moreDetails" id={accountID}><CloseButton accountID={accountID} /></div> */}
+
+            </div>
+        )
+    })
+
+    return (
+        <>
+            {accountMap}
+        </>
     )
 }
 
