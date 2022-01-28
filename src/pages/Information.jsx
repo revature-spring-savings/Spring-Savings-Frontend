36a39614e70bce66846bc4fe49dd2c88981224d4
JoCasado <jose.casado@revature.net>
2022-01-28 12:51:03 -0500

    finished information page, with styling. Thank you Hannah

--- src/pages/Information.jsx
+++ src/pages/Information.jsx
@@ -1,61 +1,45 @@
 import React from 'react';
+import "./information.scss";
 
 //be sure to add a div and use the orange, beige, and yellow color schemes from the other pages
 
 const Information = () => {
   return (
     <>
-        <h1>Pacific Trails Resort</h1>
-        <h2>Enjoy Nature in Luxury</h2>
+      <div className='content-spring'>
 
-       
+        <h1 className='content-springTitle'>Spring Savings</h1>
+        <h2 className='content-spring-Cards'>Revature's Latest Banking Application!</h2>
 
-
-        <p><span class="resort">Spring Savings</span> is a mobile-first banking application designed to be both convenient 
-          and appealing.<br/>
-          Our mission is to supply target customers with a sleek, intuitive application to meet their financial needs.
+        <p className="content-spring-area">
+          Spring Savings is a mobile-responsive banking application designed to be both convenient
+          and appealing.<br />
+          Our mission is to supply target customers with a sleek,
+          intuitive application to meet their financial needs.
         </p>
 
-        <ul>
-          <li>Private yurts with decks overlooking the ocean</li>
-          <li>Activities lodge with fireplace and gift shop</li>
-          <li>Nightly fine dining at the Overlook Cafe</li>
-          <li>Heated outdoor pool and whirlpool</li>
-          <li>Guided hiking tours of the redwoods</li>
+        <ul className="latestContent-spring">
+          <li>Secured and Tested features</li>
+          <li>Simple and Safe Banking</li>
+          <li>Flexible and Creative</li>
+          <li>Desktop and Mobile Responsive</li>
+          <li>Always Ready to help, with 24/7 Chats</li>
         </ul>
-        <br/><br/>
-          <div id="contact"><span class="resort">
-            Pacific Trails Resort</span><br/>
-              12010 Pacific Trails Road<br/>
-                Zephyr, CA 95555<br/>
-                  <br/>
-                    888-555-5555<br/>
-                      <br/>
-                      </div>
 
-                      <div id="footer"> Copyright &copy; 2018 Jose J. Casado<br/>
-                        <a href="mailto:Jose@Casado.com">Jose@Casado.com</a>
-                      </div>
-                    </>
-  )} 
-//   export default Information;
-//     <div>
-//         <h3 class="pageTitle">Store hours</h3>
-//         <p>Monday: closed</p>
-//         <p>Tues-Thurs: 10am-6pm</p>
-//         <p>Fri-Sun: 10am-8pm</p>
-//         <hr/>
-//         <h3>Address</h3>
-//         <p>123 Fake St.</p>
-//         <p>Townsburg VA, 12345</p>
-//         <hr/>
-//         <h3>Phone Number</h3>
-//         <p>(123) 123-1234</p>
-//         <hr/>
-//         <h3>Email</h3>
-//         <p>admin@thecookiejar.com</p>
-//       </div>
-//   )
-// };
+        <div className="springinfo"><span >
+          Spring Savings LLC. </span>
+          <p>7777 Spring Savings St.<br />
+            Flushing, NY 11355<br />
+
+            888-151-7474</p>
+
+          Copyright &copy; 2022 Revature - Spring Savings<br />
+          <a href="mailto:springsavings@revature.com">springsavings@revature.com</a>
+        </div>
+      </div>
+
+    </>
+  )
+}
 
 export default Information;
