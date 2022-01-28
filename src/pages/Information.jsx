afbbe2cf3262250ab1f09c2f7e9228b797689e67
JoCasado <jose.casado@revature.net>
2022-01-27 19:40:33 -0500

    working on information page

--- src/pages/Information.jsx
+++ src/pages/Information.jsx
@@ -2,13 +2,38 @@ import React from 'react';
 
 const Information = () => {
   return (
-    <div className='page-container'>
-      This is the Information Page<br/>
-      routing number?<br/>
-      contact number?<br/>
-      email?<br/>
-    </div>
-  )
-};
+    <>
+        <h1>Pacific Trails Resort</h1>
+        <h2>Enjoy Nature in Luxury</h2>
 
-export default Information;
+       
+
+
+        <p><span class="resort">Spring Savings</span> is a mobile-first banking application designed to be both convenient 
+          and appealing.<br/>
+          Our mission is to supply target customers with a sleek, intuitive application to meet their financial needs.
+        </p>
+
+        <ul>
+          <li>Private yurts with decks overlooking the ocean</li>
+          <li>Activities lodge with fireplace and gift shop</li>
+          <li>Nightly fine dining at the Overlook Cafe</li>
+          <li>Heated outdoor pool and whirlpool</li>
+          <li>Guided hiking tours of the redwoods</li>
+        </ul>
+        <br/><br/>
+          <div id="contact"><span class="resort">
+            Pacific Trails Resort</span><br/>
+              12010 Pacific Trails Road<br/>
+                Zephyr, CA 95555<br/>
+                  <br/>
+                    888-555-5555<br/>
+                      <br/>
+                      </div>
+
+                      <div id="footer"> Copyright &copy; 2018 Jose J. Casado<br/>
+                        <a href="mailto:Jose@Casado.com">Jose@Casado.com</a>
+                      </div>
+                    </>
+  )} 
+  export default Information;
