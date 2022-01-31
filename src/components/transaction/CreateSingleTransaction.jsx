45a15ac4bc7d502d8166ff7912f439998bd9a57c
stitchineer <stitchineer@yahoo.com>
2022-01-30 23:28:57 -0500

    styling and sessionstorage for transactions

--- src/components/transaction/CreateSingleTransaction.jsx
+++ src/components/transaction/CreateSingleTransaction.jsx
@@ -11,8 +11,9 @@ export default function CreateNewTransaction(props) {
     const [transactionType, setTransactionType] = useState('');
     const [transactionNote, setTransactionNote] = useState('');
     const [accountID, setAccountID] = useState(props.accountID);
+    const [acctBalance, setAcctBalance] = useState(props.accountBalance);
     const [amount, setAmount] = useState(0);
-    const [transactionBtn, setTransactionBtn] = useState(false);
+    const [userID, setUserID] = sessionStorage.getItem("userID");
 
     let newDate = new Date();
     let month = newDate.getMonth() + 1;
@@ -21,14 +22,14 @@ export default function CreateNewTransaction(props) {
     //get userID and accountID from useContext
     function changeTheValue(e) {
         setTransactionType(e);
-        setTransactionBtn(!transactionBtn);
     }
 
     function createNewTransaction() {
-        console.log(props.amount);
+        console.log(amount);
+        console.log("userID is "+userID);
         axios.post("http://ec2-54-211-135-196.compute-1.amazonaws.com:9090/transactions", [{
             accountID: accountID,
-            userID: 1,
+            userID: userID,
             amount: amount,
             transactionDate: today,
             transactionNote: transactionNote,
@@ -46,17 +47,21 @@ export default function CreateNewTransaction(props) {
         ReactDOM.render(<AccountByAcctID accountID={accountID} />, document.getElementById(accountID));
     }
 
-    function transfer(accountID){
-        ReactDOM.render(<CreateTransfer accountID={accountID}/>, document.getElementById(accountID));
+    function transfer(accountID, accountBalance){
+        ReactDOM.render(<CreateTransfer accountID={accountID} accountBalance={accountBalance} />, document.getElementById(accountID));
     }
 
     return (
         <>
+            <div className="footer-header">
+                    <h2>Account {accountID}</h2>
+                    <h3>Balance: ${acctBalance}</h3>
+                </div>
          <button  className="gray-btn" onClick={(e) => moreDetails(props.accountID)}>Go back</button>
-         <button  className="gray-btn"  onClick={(e)=>transfer(props.accountID)}>Transfer</button>
-        <br/><br/>
+         <button  className="gray-btn"  onClick={(e)=>transfer(props.accountID, props.accountBalance)}>Transfer</button>
+
             {/* <form> */}
-                Transaction <br />
+                <h3>Transaction </h3>
                 <input name="type" type="radio" id="withdraw" value="WITHDRAW" onClick={(e) => changeTheValue(e.target.value)} />
                 <label for="withdraw">Withdrawal</label>
 
@@ -64,13 +69,13 @@ export default function CreateNewTransaction(props) {
                 <label for="deposit">Deposit</label>
 
                 <br /><br />
-                Amount<br />
+                Amount:<br />
                 <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} /><br /><br />
 
-                Note<br />
+                Note:<br />
                 <input type="text" value={transactionNote} onChange={(e) => setTransactionNote(e.target.value)} placeholder="Note" /><br /><br />
 
-                <button  className="complete-btn" onClick={createNewTransaction}>{transactionBtn ? "WITHDRAW" : "DEPOSIT"}</button>
+                <button  className="complete-btn" onClick={createNewTransaction}>Finalize {transactionType}</button>
 
             {/* </form> */}
         </>
