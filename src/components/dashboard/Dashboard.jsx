258e005261ee6b5594b678dc932d3a69da084976
stitchineer <stitchineer@yahoo.com>
2022-01-31 12:36:19 -0500

    removing hardcoding for dashboard

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -3,13 +3,23 @@ import { AccountByUserID } from "../account/AccountByUserID"
 import './dashboard.scss'
 import IdleTime from '../IdleTime'
 import AccountNavbar from '../navbar/AccountNavBar';
+import { useEffect } from 'react';
+
+
+
 
 export default function Dashboard() {
+
+        if(sessionStorage.getItem('userID') != 0){
+            let username = sessionStorage.getItem("name");
+            document.getElementById("title").innerHTML = `Hello, ${username}`;
+        }
+
     return (
         <div id="dashboard-container">
             <div id="dashTitle">
                 <h1 className="pageTitle">Dashboard</h1>
-                <h3 className="title">Hello, User 2!</h3>
+                <h3 className="title"></h3>
             </div>
                 <IdleTime/>
             <div id="acctCards">
